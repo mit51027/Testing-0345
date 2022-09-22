21db7227a80e872c5813c13b191b7e58f732448e
MANISH <MANISH@DESKTOP-NPV50CU>
2022-09-22 15:10:54 +0530

    jjgjgtjg

--- GitDemo345/src/Demo.java
+++ GitDemo345/src/Demo.java
@@ -8,15 +8,8 @@ public void m1()
 System.out.println(" hello m1 method ");
 }
 
-public void m2()
-{
-System.out.println(" hello m2 method ");
-}
 
-public void m3()
-{
-System.out.println(" hello m3 method ");
-}
+
 
 public void sum()
 {
@@ -36,8 +29,8 @@ public static void main(String[] args)
 Demo a=new Demo();
 
 a.m1();
-a.m2();
-a.m3();
+
+
 a.sum();
 a.mul();
 
